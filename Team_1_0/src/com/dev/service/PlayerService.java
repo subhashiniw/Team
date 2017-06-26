95caf247d388348f1baceb41117a3346d450c3b6
subhashiniw <subhashiniwijerathna@gmail.com>
2017-06-26 18:05:30 +0530

    initial player service

---
+++ Team_1_0/src/com/dev/service/PlayerService.java
@@ -0,0 +1,13 @@
+package com.dev.service;
+
+import com.dev.dao.PlayerDAO;
+
+public class PlayerService {
+
+	private PlayerDAO playerDAO;
+	
+	public PlayerDAO getPlayerDAO() {
+		return playerDAO;
+	}
+	
+}
